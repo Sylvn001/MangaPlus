b86e8c4c542371b86676d9b520d5f21a457ff117
Junior Barros <juniorbaarros@gmail.com>
2022-07-09 18:23:52 -0300

    chore: configurated database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MangasModule } from './mangas/mangas.module';
 
+const options: object = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres123',
+  database: 'appmangas',
+  entities: [],
+  synchronize: true,
+};
+
 @Module({
-  imports: [MangasModule],
+  imports: [TypeOrmModule.forRoot(options), MangasModule],
   controllers: [AppController],
   providers: [AppService],
 })
