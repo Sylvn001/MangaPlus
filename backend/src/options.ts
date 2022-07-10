603a4215b566179ca4fb3cc9b6a2aff8c22f2a32
Junior Barros <juniorbaarros@gmail.com>
2022-07-10 15:29:13 -0300

    config: adjusted envfile

--- backend/src/options.ts
+++ backend/src/options.ts
@@ -2,8 +2,8 @@
 
 const dbOptions: object = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
