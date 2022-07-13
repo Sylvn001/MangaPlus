0449d63cf52a424960a8011be316b5510f818c80
Junior Barros <juniorbaarros@gmail.com>
2022-07-13 17:15:10 -0300

    feat: created manga entities

--- backend/src/mangas/entities/mangas.entity.ts
+++ backend/src/mangas/entities/mangas.entity.ts
@@ -1,1 +1,17 @@
-export class Mangas {}
+import { timestamp } from 'rxjs';
+import { Column, Entity, PrimaryGeneratedColumn, Timestamp } from 'typeorm';
+
+@Entity
+export class Mangas {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100 })
+  name: string;
+
+  @Column({ length: 100 })
+  img: string;
+
+  @Column({ length: 500 })
+  resume: string;
+}
--- backend/src/mangas/mangas.service.ts
+++ backend/src/mangas/mangas.service.ts
@@ -1,15 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Mangas } from './entities/mangas.entity';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateMangasDto } from './dto/create-mangas.dto';
 import { UpdateMangasDto } from './dto/update-mangas.dto';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class MangasService {
+  constructor(
+    @Inject('MANGAS_REPOSITORY')
+    private mangaRepository: Repository<Mangas>,
+  ) {}
+
   create(createMangasDto: CreateMangasDto) {
     return 'This action adds a new mangas';
   }
 
   findAll() {
-    return `This action returns all mangas`;
+    return this.mangaRepository.find();
   }
 
   findOne(id: number) {
