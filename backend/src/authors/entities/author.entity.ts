4b5c3dd30b0a5032ce96f134de2ca3ab197a97de
Junior Barros <juniorbaarros@gmail.com>
2022-07-17 13:36:44 -0300

    feat: updated entities, imprement relationships

--- backend/src/authors/entities/author.entity.ts
+++ backend/src/authors/entities/author.entity.ts
@@ -1,1 +1,22 @@
-export class Author {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Author {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 60 })
+  name: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
--- backend/src/mangas/entities/mangas.entity.ts
+++ backend/src/mangas/entities/mangas.entity.ts
@@ -1,16 +1,38 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Author } from './../../authors/entities/author.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class Mangas {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ length: 100 })
+  @Column({ length: 40 })
   name: string;
+
+  @Column({ length: 300 })
+  resume: string;
 
   @Column({ length: 100 })
   img: string;
 
-  @Column({ length: 500 })
-  resume: string;
+  @OneToOne(() => Author)
+  @JoinColumn()
+  author: Author;
+
+  @Column({ type: 'date' })
+  published_at: Date;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date;
 }
